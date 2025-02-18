b000c57672e19aa0c82077e77cda5093ed58a2eb
Suriyaprasad0303 <suriprasad03@gmail.com>
2025-02-18 13:03:57 -0500

    Add files via upload
    Class , Method and Object

---
+++ day2/Browser.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Browser {
+	
+	public void launchBrowser()
+	{
+	System.out.println("Browser Launched Successfully");
+	}
+	
+	public void loadUrl()
+	{
+	System.out.println("Application URL Loaded Successfully");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Browser obj=new Browser();
+		obj.launchBrowser();
+		obj.loadUrl();
+	}
+
+}
---
+++ day2/edgeBrowser.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class edgeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Browser ob=new Browser();
+		ob.launchBrowser();
+		ob.loadUrl();
+		
+	}
+
+}
